abd76a07067899f5e5b8f3fa010665eefa251035
Jesus Ariel <santosariel2416@gmail.com>
2026-03-22 16:21:27 -0400

    Cargue el modelo YOLOv8 y lo agregue al archivo .gitignore

---
+++ test_yolo.py
@@ -0,0 +1,8 @@
+#Jesus Ariel Santos
+#24-EISN-2-034 
+
+from ultralytics import YOLO # Prueba de carga de yolo 
+
+model = YOLO("yolov8n.pt") # Aqui se carga el modelo 
+
+print("Se instalo el modelo efectivamente")
